ed51c2cf17e063d773b0fb777404e7ec3beca068
mbed92 <michalbednarek.e@gmail.com>
2022-02-20 15:21:48 +0100

    accuracy bugfix

--- utils/clustering.py
+++ utils/clustering.py
@@ -1,29 +1,26 @@
+import io
 import os
 
+import PIL
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from scipy.optimize import linear_sum_assignment
 from sklearn.cluster import KMeans
 from torch.utils.tensorboard import SummaryWriter
+from torchvision.transforms import ToTensor
 
 
-def infer_kmeans(model, train_dataloader, test_dataloader, expected_num_clusters, device):
-    x_train = torch.cat([y[0] for y in train_dataloader], 0).type(torch.float32).permute(0, 2, 1)
-    y_train = torch.cat([y[1] for y in train_dataloader], 0).type(torch.float32)
-    emb_train = model(x_train.to(device)).detach().type(torch.float32)
-
-    x_test = torch.cat([y[0] for y in test_dataloader], 0).type(torch.float32).permute(0, 2, 1)
-    y_test = torch.cat([y[1] for y in test_dataloader], 0).type(torch.float32)
-    emb_test = model(x_test.to(device)).detach().type(torch.float32)
-
+def kmeans(x_train, x_test, expected_num_clusters):
     kmeans = KMeans(n_clusters=expected_num_clusters, n_init=20)
-    pred_train = torch.Tensor(kmeans.fit_predict(emb_train.cpu().numpy()))
-    pred_test = torch.Tensor(kmeans.predict(emb_test.cpu().numpy()))
+    return torch.Tensor(kmeans.fit_predict(x_train)), torch.Tensor(kmeans.predict(x_test))
 
+
+def measure_clustering_accuracy(y_train, y_hat_train, y_test, y_hat_test):
     print('===================')
-    print('| KMeans train accuracy:', clustering_accuracy(y_train, pred_train).numpy(),
-          '| KMeans test accuracy:', clustering_accuracy(y_test, pred_test).numpy())
+    print('| KMeans train accuracy:', clustering_accuracy(y_train, y_hat_train).numpy(),
+          '| KMeans test accuracy:', clustering_accuracy(y_test, y_hat_test).numpy())
     print('===================')
-    return emb_train, emb_test, y_train, y_test, x_train, x_test
 
 
 def clustering_accuracy(y_true: torch.Tensor, y_pred: torch.Tensor):
@@ -33,21 +30,16 @@ def clustering_accuracy(y_true: torch.Tensor, y_pred: torch.Tensor):
     indices = torch.stack([sample_idx, y_pred, y_true], 1).type(torch.int64)
 
     # add 1 when classes are ordered from 0
-    pred_idx = int(torch.max(y_pred).item())
     true_idx = int(torch.max(y_true).item())
-    if int(torch.min(y_pred).item()) == 0:
-        pred_idx += 1
     if int(torch.min(y_true).item()) == 0:
         true_idx += 1
 
-    idx = max(pred_idx, true_idx)
-    data = torch.zeros([total, idx, idx], dtype=torch.float32)
+    data = torch.zeros([total, true_idx, true_idx], dtype=torch.float32)
     indices_by_columns = [indices[:, i] for i in range(indices.shape[-1])]
-    data[indices_by_columns] = 1.
+    data[indices_by_columns] = 1.  # scatter_nd
     data = data.sum(0)
-    data = torch.max(data) - data
-    row_ind, col_ind = linear_sum_assignment(data)
-    gathered = data[row_ind, col_ind].float()
+    row_ind, col_ind = linear_sum_assignment(torch.max(data) - data)
+    gathered = data[row_ind, col_ind].float()  # gather_nd
     return torch.sum(gathered) / total
 
 
@@ -63,3 +55,21 @@ def save_embeddings(log_dir, embeddings: torch.Tensor, labels: torch.Tensor, wri
 
     torch.save({"embedding": embeddings}, os.path.join(log_dir, f"embeddings_{global_step}"))
     writer.add_embedding(embeddings, labels, global_step=global_step)
+
+
+def create_img(arr1, arr2):
+    plt.figure()
+
+    series1 = [arr1[:, i] for i in range(arr1.shape[-1])]
+    series2 = [arr2[:, i] for i in range(arr2.shape[-1])]
+    t = np.arange(0, arr1.shape[0], 1)
+    for s1, s2 in zip(series1, series2):
+        plt.plot(t, s1, 'r')
+        plt.plot(t, s2, 'g')
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    plt.close()
+    image = PIL.Image.open(buf)
+    return ToTensor()(image)[:3]
